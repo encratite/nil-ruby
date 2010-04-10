1318b86e88e46e0fa24548950e641aa89153cb33
void <git.email.address@googlemail.com>
2010-04-10 06:35:09 +0200

    Fabulous!

--- string.rb
+++ string.rb
@@ -1,15 +1,20 @@
-module Nil
-	def self.extractString(input, left, right, occurence = 1)
+class String
+	def extract(left, right, occurence = 1)
 		offset = 0
 		while occurence > 0
-			offset = input.index(left, offset)
+			offset = index(left, offset)
 			return nil if offset == nil
 			offset += left.size
 			occurence -= 1
 		end
-		rightOffset = input.index(right, offset)
+		rightOffset = index(right, offset)
 		return nil if rightOffset == nil
-		output = input[offset..(rightOffset - 1)]
+		output = self[offset..(rightOffset - 1)]
 		return output
+	end
+	
+	def isNumber
+		pattern = /^-?((0|[1-9]\d*)(\.\d*)?|\.\d+)$/
+		return pattern.match(self) != nil
 	end
 end
