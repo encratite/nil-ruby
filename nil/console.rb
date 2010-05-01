16ea976b2e5a1fb6b6b0bd08924c4e8da87da36c
void <git.email.address@googlemail.com>
2010-05-02 00:10:02 +0200

    Added UNIX console colours stuff

---
+++ nil/console.rb
@@ -0,0 +1,23 @@
+module Nil
+	module Console
+		LightGrey = "\e[0;37m"
+		LightRed = "\e[1;31m"
+		LightGreen = "\e[1;32m"
+		LightBlue = "\e[1;34m"
+		LightCyan = "\e[1;36m"
+		DarkGrey = "\e[1;30m"
+		
+		Black = "\e[0;30m"
+		Red = "\e[0;31m"
+		Green = "\e[0;32m"
+		Brown = "\e[0;33m"
+		Blue = "\e[0;34m"
+		Purple = "\e[0;35m"
+		Cyan = "\e[0;36m"
+		Yellow = "\e[1;33m"
+		Pink = "\e[1;35m"
+		White = "\e[1;37m"
+		
+		Normal = "\e[0m"
+	end
+end
