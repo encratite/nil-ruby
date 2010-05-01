8ffa5d198580356599ca288107deb0507951b2be
void <git.email.address@googlemail.com>
2010-05-01 02:28:46 +0200

    Added functions to retrieve the space remaining on a device

--- nil/file.rb
+++ nil/file.rb
@@ -1,3 +1,6 @@
+require 'nil/string'
+require 'nil/environment'
+
 module Nil
 	def self.readFile(path)
 		begin
@@ -23,6 +26,26 @@ module Nil
 			file.close
 		rescue Errno::EINVAL
 			return nil
+		end
+	end
+	
+	def self.getFreeSpace(path)
+		if getOS == :windows
+			data = `dir #{path}`.split("\n")
+			raise StandardError.new 'Empty directory listing output' if data.empty?
+			data = data[-1].gsub(',', '')
+			match = /\d+.+?(\d+)/.match(data)
+			raise StandardError.new 'Unable to match free space pattern' if match == nil
+			return match[1].to_i
+		else
+			data = `df -Pk`.split("\n")
+			raise StandardError.new 'Invalid line count from df' if data.size != 2
+			data = data[1].split(' ')
+			raise StandardError.new 'Invalid token count from df' if data.size != 6
+			data = data[3]
+			raise StandardError.new 'Invalid size from df' if !data.isNumber
+			output = data.to_i * 1024
+			return output
 		end
 	end
 end
