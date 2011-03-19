baaca41cc232b419cad22dcd0852d4aef10dfe2b
void <git.email.address@googlemail.com>
2011-03-19 21:54:28 +0100

    Extended the symbolic assignment module

--- nil/symbol.rb
+++ nil/symbol.rb
@@ -1,6 +1,15 @@
 module SymbolicAssignment
+  def self.translateSymbol(symbol)
+    return ('@' + symbol.to_s).to_sym
+  end
+
+  def getMember(symbol)
+    symbol = SymbolicAssignment.translateSymbol(symbol)
+    return instance_variable_get(symbol)
+  end
+
   def setMember(symbol, value)
-    symbol = ('@' + symbol.to_s).to_sym
+    symbol = SymbolicAssignment.translateSymbol(symbol)
     instance_variable_set(symbol, value)
   end
 end
