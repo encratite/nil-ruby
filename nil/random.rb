55e69afe2f3bca3f11b3189b3761f4cc85bd28b7
void <git.email.address@googlemail.com>
2011-07-18 15:10:25 +0200

    Added a random scale

--- nil/random.rb
+++ nil/random.rb
@@ -1,4 +1,36 @@
 module Nil
+  class RandomScaleElement
+    attr_reader :element, :weight
+
+    def initialize(element, weight)
+      @element = element
+      @weight = weight
+    end
+  end
+
+  class RandomScale
+    def initialize
+      @elements = []
+      @totalWeight = 0
+    end
+
+    def add(element, weight)
+      @elements << RandomScaleElement.new(element, weight)
+      @totalWeight += weight
+    end
+
+    def get
+      choice = rand(@totalWeight)
+      @elements.each do |element|
+        if choice < element.weight
+          return element.element
+        end
+        choice -= element.weight
+      end
+      raise 'Failed to retrieve an element'
+    end
+  end
+
   def self.randomInteger(minimum, maximum)
     return minimum + rand(maximum - minimum)
   end
