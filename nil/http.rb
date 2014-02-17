ee6fb8f470fc6ab7bd7fc9a6084e02a9e173726c
void <git.email.address@googlemail.com>
2014-02-17 01:21:32 +0100

    Catch 'em all, wee

--- nil/http.rb
+++ nil/http.rb
@@ -81,7 +81,7 @@ module Nil
           return get(newPath, path)
         end
         return response.body
-      rescue SystemCallError, Net::ProtocolError, RuntimeError, IOError, SocketError, OpenSSL::SSL::SSLError => exception
+      rescue exception
         puts "GET exception: #{exception.inspect}"
         return nil
       end
